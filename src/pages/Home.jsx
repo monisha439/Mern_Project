c52f89d2de82ec6c65e4d9b30886a1ec7eae7666
monisha439 <monishavelu333@gmail.com>
2025-02-23 17:32:02 +0530

    Add files via upload

---
+++ src/pages/Home.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+    <img src="/ban3.jpg" class="img-fluid w-100 pt-1" alt="..." />
+
+
+     <div class="row row-cols-1 row-cols-md-2 g-4 m-2">
+  <div class="col">
+    <div class="card">
+      <img src="/ban1.jpg" class="card-img-top" alt="..."/>
+      <div class="card-body">
+        <h5 class="card-title">Veg Recipes</h5>
+        <p class="card-text">The measured ingredients in a recipe not only produce consistent food but control your food cost and profit. Recipes reduce waste because a cook is prepping exactly what is needed to produce the menu items. Recipes provide portion control which is a major factor in food cost control and profit.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/ban12.jpg" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="card-title">Non-Veg</h5>
+        <p class="card-text">Delicious meals are tasty, appetizing, scrumptious, yummy, luscious, delectable, mouth-watering, fit for a king, delightful, lovely, wonderful, pleasant, enjoyable, appealing, enchanting, charming and hihgly pleasant to the taste.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/ban2.jpg" class="card-img-top" alt="..."/ >
+      <div class="card-body">
+        <h5 class="card-title">Bread</h5>
+        <p class="card-text">The purpose of a recipe is to provide a written guide to make cooking a particular dish easier. But recipes designed in this way don't always work for our children, especially if they struggle to read or find the maths of measurement difficult.</p>
+      </div>
+    </div>
+  </div>
+  <div class="col">
+    <div class="card">
+      <img src="/ban.jpg" class="card-img-top" alt="..." />
+      <div class="card-body">
+        <h5 class="card-title">Veg</h5>
+        <p class="card-text">Food is one of the basic necessities of life. Food contains nutrients—substances essential for the growth, repair, and maintenance of body tissues and for the regulation of vital processes.</p>
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+  )
+}
+
+export default Home
